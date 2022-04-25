123ef77e82496f4fc795bb5193ec8e50217ccb20
Jacob Gutierrez <jgutier7@student06.cse.nd.edu>
2022-04-25 16:14:56 -0400

    bcubed.cpp: added usage function

--- src/bcubed.cpp
+++ src/bcubed.cpp
@@ -3,18 +3,35 @@
  * Emails: dbrahmbh@nd.edu, jgutier7@nd.edu, jmuleya@nd.edu, dbalsar1@nd.edu
  * File Name: bcubed.cpp
  * Date Created: 4/12/2022
+ * Date Edited: 4/25/2022
  * File Contents: this file contains the function definitons for our b-cubed solver
  ************************************************/
 #include "../include/bcubed.h"
 
+// usage function
+void usage(int argc,char *progname) {
+    if(argc==1) {
+        std::cerr << "ERROR: Too few arguments" << ENDL;
+    } else {
+        std::cerr << "ERROR: Too many arguments" << ENDL;
+    }
+     std::cerr << "Usage: B-CUBED SOLVER requires two arguments:"  << ENDL << 
+     "                    * executable " << ENDL <<
+     "                    * a text file containing the data for the level." << ENDL << ENDL <<
+     "         " << progname << " [level].txt" << ENDL << ENDL;
+     exit(1);
+}
 
+/*
+// reads board from input file 
 void read_level(Board& board, IFSTREAM& input_file){
     
 } // 
 
+
 void store_data(Board& board, UNOR_MAP<int, VECTOR<int>>& solver_data){
 
-} /*
+} 
 
 //create seperate files that contain the include statement so we can all work on different 
 //parts
