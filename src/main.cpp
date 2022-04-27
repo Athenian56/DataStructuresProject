15e73cba422f2ccbdd52a57916e330b416445d98
Jacob Gutierrez <jgutier7@student05.cse.nd.edu>
2022-04-27 15:07:14 -0400

    Update: Read into Main

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,30 @@ int main(int argc, char *argv[]){
     if(argc != 2){  //sends to function telling user how to use main
         usage(argc,argv[0]); //tells the user that they ned more than one argument 
     } 
+
+    /* Read Level Input */
+	IFSTREAM bcubed_infile;
+    Board board; //intilizes board
+    bcubed_infile.open( argv[1] ); //opens the input
+    read_level(board,bcubed_infile); //reads level
+
     /* Solve Puzzle */
+    UNOR_MAP<int, VECTOR<int>> solver_data;
+    store_data(board, solver_data);
+
+    /* DEBUGGING */ 
+    COUT << "hash table" << ENDL;
+    COUT << "Index\t\tDestins" << ENDL;
+    for(long unsigned int i = 0; i < solver_data.size(); i++){
+        COUT << "  " << std::setw(2) << i << "\t\t";
+        for(long unsigned int j = 0; j < solver_data[(int)i].size(); j++){
+            if (j != 0)
+                COUT << ", ";
+            COUT << std::setw(2) << solver_data[(int)i][j];
+        }
+        COUT << ENDL;
+    }
+    /* DEBUGGING */
 
 
     /* Return 0 if Successful */
